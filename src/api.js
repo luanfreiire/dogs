b86fed6144acd3b2cd1402128790ea7d23abf047
devluanfreire <luuanfreeire@gmail.com>
2025-11-24 20:02:17 -0300

    feat(photo): adiciona pagina de fotos unica

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
-export const API_URL = "https://dogsapi.origamid.dev/json/";
+export const API_URL = "https://dogsapi.origamid.dev/json";
 
 export function TOKEN_POST(body) {
   return {
-    url: API_URL + "jwt-auth/v1/token",
+    url: API_URL + "/jwt-auth/v1/token",
     options: {
       method: "POST",
       headers: {
@@ -15,7 +15,7 @@ export function TOKEN_POST(body) {
 
 export function TOKEN_VALIDATE_POST(token) {
   return {
-    url: API_URL + "jwt-auth/v1/token/validate",
+    url: API_URL + "/jwt-auth/v1/token/validate",
     options: {
       method: "POST",
       headers: {
@@ -27,7 +27,7 @@ export function TOKEN_VALIDATE_POST(token) {
 
 export function USER_GET(token) {
   return {
-    url: API_URL + "api/user",
+    url: API_URL + "/api/user",
     options: {
       method: "GET",
       headers: {
@@ -39,7 +39,7 @@ export function USER_GET(token) {
 
 export function USER_POST(body) {
   return {
-    url: API_URL + "api/user",
+    url: API_URL + "/api/user",
     options: {
       method: "POST",
       headers: {
@@ -52,7 +52,7 @@ export function USER_POST(body) {
 
 export function PHOTO_POST(formData, token) {
   return {
-    url: API_URL + "api/photo",
+    url: API_URL + "/api/photo",
     options: {
       method: "POST",
       headers: {
@@ -65,7 +65,7 @@ export function PHOTO_POST(formData, token) {
 
 export function PHOTOS_GET({ page, total, user }) {
   return {
-    url: `${API_URL}api/photo/?_page=${page}&_total=${total}&_user=${user}`,
+    url: `${API_URL}/api/photo/?_page=${page}&_total=${total}&_user=${user}`,
     options: {
       method: "GET",
       cache: "no-store",
@@ -75,7 +75,7 @@ export function PHOTOS_GET({ page, total, user }) {
 
 export function PHOTO_GET(id) {
   return {
-    url: `${API_URL}api/photo/${id}`,
+    url: `${API_URL}/api/photo/${id}`,
     options: {
       method: "GET",
       cache: "no-store",
@@ -85,7 +85,7 @@ export function PHOTO_GET(id) {
 
 export function COMMENT_POST(id, body) {
   return {
-    url: `${API_URL}api/comment/${id}`,
+    url: `${API_URL}/api/comment/${id}`,
     options: {
       method: "POST",
       headers: {
