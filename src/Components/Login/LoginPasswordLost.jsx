fb318c1e56b6f927df05ba3bcbe1d2d63f16da04
devluanfreire <luuanfreeire@gmail.com>
2025-11-25 09:14:25 -0300

    feat(LoginPasswordLost): funcao de perdeu senha e resetar senha

--- src/Components/Login/LoginPasswordLost.jsx
+++ src/Components/Login/LoginPasswordLost.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from "react";
+import Input from "../Forms/Input";
+import Button from "../Forms/Button";
+import Error from "../Helper/Error";
+import useForm from "../../Hooks/useForm";
+import useFetch from "../../Hooks/useFetch";
+import { PASSWORD_LOST } from "../../api";
 
 const LoginPasswordLost = () => {
-  return (
-    <div>
-      Login Password Lost
-    </div>
-  )
-}
+  const login = useForm();
+  const { data, loading, error, request } = useFetch();
 
-export default LoginPasswordLost
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    if (login.validate()) {
+      const { url, options } = PASSWORD_LOST({
+        login: login.value,
+        url: window.location.href.replace("perdeu", "resetar"),
+      });
+      request(url, options);
+    }
+  }
+
+  return (
+    <section>
+      <h1 className="title">Perdeu sua senha ?</h1>
+      {data ? (
+        <p style={{ color: "#4c1" }}>{data}</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <Input label="Email / Usuário" type="text" name="login" {...login} />
+          {loading ? <Button>Enviando</Button> : <Button>Enviar Email</Button>}
+        </form>
+      )}
+      <Error error={error} />
+    </section>
+  );
+};
+
+export default LoginPasswordLost;
